48dcf3145b3b8a287b8cece676549b453c184977
Paulo Andrade <parthur207@gmail.com>
2026-08-01 12:41:23 -0300

    Migrations.

--- TaskManager.Adapters/Persistence/Migrations/20260729231142_UpdateDb.Designer.cs
+++ TaskManager.Adapters/Persistence/Migrations/20260729231142_UpdateDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TaskManager.Adapters.Persistence;
 
@@ -11,9 +12,11 @@ using TaskManager.Adapters.Persistence;
 namespace TaskManager.Adapters.Migrations
 {
     [DbContext(typeof(DbContextTaskManager))]
-    partial class DbContextTaskManagerModelSnapshot : ModelSnapshot
+    [Migration("20260729231142_UpdateDb")]
+    partial class UpdateDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TaskManager.Adapters/Persistence/Migrations/20260729231142_UpdateDb.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManager.Adapters.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "TwoFactores",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExecutionPlan",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "JustifyPriority",
+                table: "Tasks",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PrioritySort",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TwoFactores",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ExecutionPlan",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "JustifyPriority",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "PrioritySort",
+                table: "Tasks");
+        }
+    }
+}
